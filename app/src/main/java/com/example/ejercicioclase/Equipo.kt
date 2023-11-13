54d28dac5abfdb6b72d2b32e7d84b939054c4385
EAG <rauulssanchezz@gmail.com>
2023-11-13 19:54:13 +0100

    fghj

--- app/src/main/java/com/example/ejercicioclase/Equipo.kt
+++ app/src/main/java/com/example/ejercicioclase/Equipo.kt
@@ -9,9 +9,23 @@ class Equipo {
         this.nombre=nombre
         this.equipo=equipo
     }
-}
-
-fun championsLeague(lista:MutableList<Equipo>){
-
 
 }
+
+fun  championsLeague(lista:MutableList<Equipo>):String{
+
+   var equipos=lista
+    var local:Equipo
+    var visitante:Equipo
+    var res=""
+    for (i in 1..equipos.size/2){
+        local=equipos.random()
+        equipos.remove(local)
+        visitante=equipos.random()
+        equipos.remove(visitante)
+        res+="${local.nombre} vs ${visitante.nombre}\n"
+    }
+    return res
+}
+
+
--- app/src/main/java/com/example/ejercicioclase/Main.kt
+++ app/src/main/java/com/example/ejercicioclase/Main.kt
@@ -112,57 +112,59 @@ fun main(){
      */
 
     //Ejercicio6
-    var lista= mutableListOf<Planeta>(
-        Planeta("Ano","Gaseoso",20.5),
-        Planeta("Tierra","Rocoso",2.5),
-        Planeta("Jupiter","Gaseoso",0.5),
-        Planeta("Marte","rocoso",0.5)
-    )
-    //Ejercicio7
-    for (i in lista.indices){
-        println(lista[i].toString())
-    }
-
-    //Ejercicio8
-    for (i in lista.indices){
-        if(lista[i].nombre[0].equals('m',true)){
-            println(lista[i].toString())
-        }
-    }
+//    var lista= mutableListOf<Planeta>(
+//        Planeta("Ano","Gaseoso",20.5),
+//        Planeta("Tierra","Rocoso",2.5),
+//        Planeta("Jupiter","Gaseoso",0.5),
+//        Planeta("Marte","rocoso",0.5)
+//    )
+//    //Ejercicio7
+//    for (i in lista.indices){
+//        println(lista[i].toString())
+//    }
+//
+//    //Ejercicio8
+//    for (i in lista.indices){
+//        if(lista[i].nombre[0].equals('m',true)){
+//            println(lista[i].toString())
+//        }
+//    }
 
     //Ejercicio9
-    for (i in lista.indices){
-        if (lista[i].tipo.equals("gaseoso",true)&&lista[i].masa>20){
-            println(lista[i].toString())
-        }
-    }
-
-    //Ejercicio10
-    var media=0.0
-    var suma=0.0
-    for (i in lista.indices){
-        suma+=lista[i].masa
-    }
-    media=suma/lista.size
-    println(media)
+//    for (i in lista.indices){
+//        if (lista[i].tipo.equals("gaseoso",true)&&lista[i].masa>20){
+//            println(lista[i].toString())
+//        }
+//    }
+//
+//    //Ejercicio10
+//    var media=0.0
+//    var suma=0.0
+//    for (i in lista.indices){
+//        suma+=lista[i].masa
+//    }
+//    media=suma/lista.size
+//    println(media)
 
     //Ejercicio12
     var furbito= mutableListOf<Equipo>(
+
         Equipo("Madri","España"),
-                Equipo("Madri","España"),
-    Equipo("Madri","España"),
-    Equipo("Barca","España"),
-    Equipo("PSG","Francia"),
-    Equipo("Bayern","Alemania"),
-    Equipo("Porto","Portugal"),
-    Equipo("City","Inglaterra"),
-    Equipo("Chealse","Inglaterra"),
-    Equipo("Dormunt","Alemania"),
-    Equipo("Lyon","Francia"),
-            Equipo("Galatasaray","Turquia"),
+        Equipo("Barca","España"),
+        Equipo("PSG","Francia"),
+        Equipo("Bayern","Alemania"),
+        Equipo("Porto","Portugal"),
+        Equipo("City","Inglaterra"),
+        Equipo("Chealse","Inglaterra"),
+        Equipo("Dormunt","Alemania"),
+        Equipo("Lyon","Francia"),
+        Equipo("Galatasaray","Turquia"),
         Equipo("Benfica","Portugal"),
         Equipo("Napoles","Italia"),
         Equipo("Milan","Italia"),
-        Equipo("Juventus","Italia")
+        Equipo("Juventus","Italia"),
+        Equipo("Nose","jaja"),
+        Equipo("Nose2","jaja2")
     )
+    println(championsLeague(furbito))
 }
